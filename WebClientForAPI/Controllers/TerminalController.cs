7e1a3e2038730e4765279862eacaa99b06a655e0
DeveloperCramp <biodubstep@gmail.com>
2022-07-01 23:22:03 +0300

    under work...

---
+++ WebClientForAPI/Controllers/TerminalController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebClientForAPI.Controllers
+{
+
+    public class TerminalController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public TerminalController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            string token = (string)TempData["token"];
+            var client = _httpClientFactory.CreateClient("ClientAPI");
+            var request = await client.GetAsync($"commands/types?token={token}");
+
+            if (!request.IsSuccessStatusCode)
+            {
+                return View(request);
+            }
+
+            return View();
+        }
+    }
+}
