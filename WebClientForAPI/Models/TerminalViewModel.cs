6de4683930b4d12923ba4fbf8a0901efe0f1bf83
DeveloperCramp <biodubstep@gmail.com>
2022-07-01 23:20:01 +0300

    Add TerminalViewModel

---
+++ WebClientForAPI/Models/TerminalViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WebClientForAPI.Models
+{
+    public class TerminalViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Parameter_name1 { get; set; }
+        public string Parameter_name2 { get; set; }
+        public string Parameter_name3 { get; set; }
+        public string Str_parameter_name1 { get; set; }
+        public string Str_parameter_name2 { get; set; }
+        public string Parameter_default_value1 { get; set; }
+        public string Parameter_default_value2 { get; set; }
+        public string Parameter_default_value3 { get; set; }
+    }
+}
