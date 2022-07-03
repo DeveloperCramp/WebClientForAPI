1b62d3374bd156b8b517b312b55743b325300135
DeveloperCramp <biodubstep@gmail.com>
2022-07-03 17:49:50 +0300

    Edit

--- WebClientForAPI/Models/TerminalViewModel.cs
+++ WebClientForAPI/Models/TerminalViewModel.cs
@@ -1,13 +1,20 @@
-﻿namespace WebClientForAPI.Models
+﻿using System.Collections.Generic;
+
+namespace WebClientForAPI.Models
 {
     public class TerminalViewModel
     {
+        public int Page_number { get; set; }
+        public int Items_per_page { get; set; }
+        public int Items_Count { get; set; }
+        public List<TerminalViewModel> Items { get; set; }
+        public bool Success { get; set; }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int Parameter1 { get; set; }
         public int Parameter2 { get; set; }
         public int Parameter3 { get; set; }
-
         public string Parameter_name1 { get; set; }
         public string Parameter_name2 { get; set; }
         public string Parameter_name3 { get; set; }
